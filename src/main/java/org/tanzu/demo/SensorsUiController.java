cae055befd3921fd359c6e69a5b5eb2b97f9a53c
dbha <dbha0719@gmail.com>
2022-11-09 21:59:12 +0900

    update

--- src/main/java/org/tanzu/demo/SensorsUiController.java
+++ src/main/java/org/tanzu/demo/SensorsUiController.java
@@ -1,15 +1,15 @@
 package org.tanzu.demo;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RestController
+@Controller
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SensorsUiController {
 
